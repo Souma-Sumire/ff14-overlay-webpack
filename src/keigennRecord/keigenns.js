60076587a7196ccc403e6ac87222ca92cee96f40
Souma <553469159@qq.com>
2023-01-24 04:30:28 +0800

    🌈 style:

--- src/keigennRecord/keigenns.js
+++ src/keigennRecord/keigenns.js
@@ -1,20 +1,20 @@
 let keigenns = {
   "4a7": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //铁壁
-  740: { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //盾阵
-  496: { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //干预
+  "740": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //盾阵
+  "496": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //干预
   "4a": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //预警
-  498: { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //武装
-  52: { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //神圣领域
+  "498": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //武装
+  "52": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //神圣领域
   "2d7": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //圣光幕帘（已触发）
   "a72": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //ホーリーシェルトロン 82级新技能
   "a73": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //ナイトの堅守 82级新技能 新盾阵
 
   "2df": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //原初的直觉
   "8b3": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //原初的勇猛
-  57: { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //战栗
+  "57": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //战栗
   // "59": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //复仇
   "5b1": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //摆脱
-  199: { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //死斗
+  "199": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //死斗
   "a76": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //原初の血気 82级新技能
   "a77": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //原初の血潮 82级新技能
   "a78": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //原初の血煙 82级新技能
@@ -22,14 +22,14 @@ let keigenns = {
   "49a": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //至黑之夜
   "2ea": { dodge: 0, physics: 0, magic: 1, darkness: 0, condition: "player" }, //弃明投暗
   "2eb": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //暗影墙
-  766: { dodge: 0, physics: 0, magic: 1, darkness: 0, condition: "player" }, //暗黑布道
+  "766": { dodge: 0, physics: 0, magic: 1, darkness: 0, condition: "player" }, //暗黑布道
   "32a": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //行尸走肉
-  811: { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //死而不僵
+  "811": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //死而不僵
   "a7a": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //オブレーション 82级新技能
 
-  730: { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //石之心
+  "730": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //石之心
   "76a": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //残暴弹
-  728: { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //伪装
+  "728": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //伪装
   "72a": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //星云
   "72f": { dodge: 0, physics: 0, magic: 1, darkness: 0, condition: "player" }, //光之心
   "72c": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //超火流星
@@ -37,27 +37,27 @@ let keigenns = {
   "a7c": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //クラリティー・オブ・コランダム 82级新技能
 
   "4c2": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //神祝祷
-  751: { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //节制
+  "751": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //节制
   "a94": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //水幕帘
 
-  129: { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //鼓舞
+  "129": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //鼓舞
   "77e": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //激励
   "77d": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //炽天的幕帘
   "12b": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //野战治疗阵
   "13d": { dodge: 0, physics: 0, magic: 1, darkness: 0, condition: "player" }, //异想的幻光
-  753: { dodge: 0, physics: 0, magic: 1, darkness: 0, condition: "player" }, //炽天的幻光
+  "753": { dodge: 0, physics: 0, magic: 1, darkness: 0, condition: "player" }, //炽天的幻光
   "a96": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //生命回生法
   "a97": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //怒濤の計
 
-  351: { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //命运之轮
+  "351": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //命运之轮
   "4b6": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //国服夜占命运之轮
-  761: { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //天星交错
+  "761": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //天星交错
   "a9d": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //エクザルテーション 82级新技能
-  781: { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //中间学派
+  "781": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //中间学派
   "7fb": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //国服中间学派（夜）
-  758: { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //国服天星冲日（夜）
-  817: { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //国服天星冲日（夜）
-  345: { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //国服黑夜领域
+  "758": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //国服天星冲日（夜）
+  "817": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //国服天星冲日（夜）
+  "345": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //国服黑夜领域
 
   "a2f": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //エウクラシア・ディアグノシス 贤单盾
   "a30": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //エウクラシア・ディアグノシス[強] 贤单盾暴击
@@ -72,11 +72,11 @@ let keigenns = {
   "49b": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //金刚极意
   "78e": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //行吟
   "79f": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //策动
-  722: { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //防守之桑巴
+  "722": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //防守之桑巴
   "a89": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //即兴表演结束
-  
+
   "1e8": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //残影
-  a8: { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //魔罩
+  "a8": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //魔罩
   "a8e": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //守りの光
   "a25": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //守護のクレスト 钐镰魔罩
   "a93": { dodge: 0, physics: 0, magic: 1, darkness: 0, condition: "player" }, //バマジク 赤魔新86级群减
@@ -91,15 +91,15 @@ let keigenns = {
   "4b3": { dodge: 1, physics: 0.5, magic: 1, darkness: 0, condition: "enemy" }, //昏乱
 
   //青魔
-  848: { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //体力增加（捕食）
+  "848": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //体力增加（捕食）
   "6b3": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "enemy" }, //腐臭（臭气）
-  843: { dodge: 0, physics: 0, magic: 1, darkness: 0, condition: "enemy" }, //智力精神降低（魔法锤）
+  "843": { dodge: 0, physics: 0, magic: 1, darkness: 0, condition: "enemy" }, //智力精神降低（魔法锤）
   "9c4": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //龙之力
   "6ba": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //超硬化
   "9c0": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //玄结界
-  847: { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //仙人盾
+  "847": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //仙人盾
   "6b7": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //强力守护
-  842: { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //哥布防御
+  "842": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //哥布防御
 
   //TLB
   "c2": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //铜墙铁盾
