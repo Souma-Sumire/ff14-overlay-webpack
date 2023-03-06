3d4a28e4e5084749c6e34d0c2e3ad0d4180fb688
Souma <553469159@qq.com>
2023-03-07 04:40:55 +0800

    修复机工武装解除失效的问题

--- src/keigennRecord/keigenns.js
+++ src/keigennRecord/keigenns.js
@@ -1,114 +1,91 @@
-let keigenns = {
-  "4a7": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //铁壁
-  "740": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //盾阵
-  "496": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //干预
-  "4a": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //预警
-  "498": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //武装
-  "52": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //神圣领域
-  "2d7": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //圣光幕帘（已触发）
-  "a72": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //ホーリーシェルトロン 82级新技能
-  "a73": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //ナイトの堅守 82级新技能 新盾阵
-
-  "2df": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //原初的直觉
-  "8b3": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //原初的勇猛
-  "57": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //战栗
-  // "59": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //复仇
-  "5b1": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //摆脱
-  "199": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //死斗
-  "a76": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //原初の血気 82级新技能
-  "a77": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //原初の血潮 82级新技能
-  "a78": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //原初の血煙 82级新技能
-
-  "49a": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //至黑之夜
-  "2ea": { dodge: 0, physics: 0, magic: 1, darkness: 0, condition: "player" }, //弃明投暗
-  "2eb": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //暗影墙
-  "766": { dodge: 0, physics: 0, magic: 1, darkness: 0, condition: "player" }, //暗黑布道
-  "32a": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //行尸走肉
-  "811": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //死而不僵
-  "a7a": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //オブレーション 82级新技能
-
-  "730": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //石之心
-  "76a": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //残暴弹
-  "728": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //伪装
-  "72a": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //星云
-  "72f": { dodge: 0, physics: 0, magic: 1, darkness: 0, condition: "player" }, //光之心
-  "72c": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //超火流星
-  "a7b": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //ハート・オブ・コランダム 82级新技能
-  "a7c": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //クラリティー・オブ・コランダム 82级新技能
-
-  "4c2": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //神祝祷
-  "751": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //节制
-  "a94": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //水幕帘
-
-  "129": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //鼓舞
-  "77e": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //激励
-  "77d": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //炽天的幕帘
-  "12b": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //野战治疗阵
-  "13d": { dodge: 0, physics: 0, magic: 1, darkness: 0, condition: "player" }, //异想的幻光
-  "753": { dodge: 0, physics: 0, magic: 1, darkness: 0, condition: "player" }, //炽天的幻光
-  "a96": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //生命回生法
-  "a97": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //怒濤の計
-
-  "351": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //命运之轮
-  // "4b6": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //国服夜占命运之轮
-  "761": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //天星交错
-  "a9d": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //エクザルテーション 82级新技能
-  "781": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //中间学派
-  // "7fb": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //国服中间学派（夜）
-  // "758": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //国服天星冲日（夜）
-  // "817": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //国服天星冲日（夜）
-  // "345": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //国服黑夜领域
-
-  "a2f": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //エウクラシア・ディアグノシス 贤单盾
-  "a30": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //エウクラシア・ディアグノシス[強] 贤单盾暴击
-  "a31": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //エウクラシア・プログノシス 贤群盾
-  "a3a": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //ケーラコレ 贤罩子
-  "a3b": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //タウロコレ 贤绿帽
-  "bbb": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //ホーリズム 整体论
-  "d25": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //整体盾
-  "a52": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //海马
-  "a53": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //胖海马
-
-  "4d0": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //心眼
-  "49b": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //金刚极意
-  "78e": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //行吟
-  "79f": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //策动
-  "722": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //防守之桑巴
-  "a89": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //即兴表演结束
-  "35c": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //武装解除
-
-  "1e8": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //残影
-  "a8": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //魔罩
-  "a8e": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //守りの光
-  "a25": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //守護のクレスト 钐镰魔罩
-  "a93": { dodge: 0, physics: 0, magic: 1, darkness: 0, condition: "player" }, //バマジク 赤魔新86级群减
-  // "2ab": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //土之加护
-  // "790": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //国服防护障壁
-
-  //ClassJob
-  "4b9": { dodge: 1, physics: 1, magic: 0, darkness: 0, condition: "player" }, //亲疏自行
-  // "09": { dodge: 1, physics: 1, magic: 0, darkness: 0, condition: "enemy" }, //减速
-  "4a9": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "enemy" }, //雪仇
-  "4ab": { dodge: 1, physics: 1, magic: 0.5, darkness: 0, condition: "enemy" }, //牵制
-  "4b3": { dodge: 1, physics: 0.5, magic: 1, darkness: 0, condition: "enemy" }, //昏乱
-
-  //青魔
-  "848": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //体力增加（捕食）
-  "6b3": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "enemy" }, //腐臭（臭气）
-  "843": { dodge: 0, physics: 0, magic: 1, darkness: 0, condition: "enemy" }, //智力精神降低（魔法锤）
-  "9c4": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //龙之力
-  "6ba": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //超硬化
-  "9c0": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //玄结界
-  "847": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //仙人盾
-  "6b7": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //强力守护
-  "842": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //哥布防御
-
-  //TLB
-  "c2": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //铜墙铁盾
-  "c3": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //坚守要塞
-  "c4": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //终极壁垒
-  "35f": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //原初大地
-  "360": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //暗黑之力
-  "78b": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //灵魂之青
+const keigenns = {
+  "4a7": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 铁壁
+  "740": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 盾阵
+  "496": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 干预
+  "4a": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 预警
+  "498": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 武装
+  "52": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 神圣领域
+  "2d7": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 圣光幕帘
+  "a72": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 圣盾阵
+  "a73": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 骑士的坚守
+  "2df": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 原初的直觉
+  "8b3": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 原初的勇猛
+  "57": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 战栗
+  "5b1": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 摆脱
+  "199": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 死斗
+  "a76": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 原初的血气
+  "a77": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 原初的血潮
+  "a78": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 原初的血烟
+    // "59": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, //复仇 "受伤减轻"已经包括在默认buff中
+  "49a": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 至黑之夜
+  "2ea": { dodge: 0, physics: 0, magic: 1, darkness: 0, condition: "player" }, // 弃明投暗
+  "2eb": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 暗影墙
+  "766": { dodge: 0, physics: 0, magic: 1, darkness: 0, condition: "player" }, // 暗黑布道
+  "32a": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 行尸走肉
+  "811": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 极光
+  "a7a": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 献奉
+  "730": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 石之心
+  "76a": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 残暴弹
+  "728": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 伪装
+  "72a": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 星云
+  "72f": { dodge: 0, physics: 0, magic: 1, darkness: 0, condition: "player" }, // 光之心
+  "72c": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 超火流星
+  "a7b": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 刚玉之心
+  "a7c": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 刚玉之清
+  "4c2": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 神祝祷
+  "751": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 节制
+  "a94": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 水流幕
+  "129": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 鼓舞
+  "77e": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 激励
+  "77d": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 炽天的幕帘
+  "12b": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 野战治疗阵
+  "13d": { dodge: 0, physics: 0, magic: 1, darkness: 0, condition: "player" }, // 异想的幻光
+  "753": { dodge: 0, physics: 0, magic: 1, darkness: 0, condition: "player" }, // 炽天的幻光
+  "a96": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 生命回生法
+  "a97": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 怒涛之计
+  "351": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 命运之轮
+  "761": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 天星交错
+  "a9d": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 擢升
+  "781": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 中间学派
+  "a2f": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 均衡诊断
+  "a30": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 齐衡诊断
+  "a31": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 均衡预后
+  "a3a": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 坚角清汁
+  "a3b": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 白牛清汁
+  "bbb": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 整体论
+  "d25": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 整体盾
+  "a52": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 血印
+  "a53": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 泛血印
+  "4d0": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 心眼
+  "49b": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 金刚极意
+  "78e": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 行吟
+  "79f": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 策动
+  "722": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 防守之桑巴
+  "a89": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 即兴表演结束
+  "1e8": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 残影
+  "a8": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 魔罩
+  "a8e": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 守护之光
+  "a25": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 守护纹
+  "a93": { dodge: 0, physics: 0, magic: 1, darkness: 0, condition: "player" }, // 抗死
+  "4b9": { dodge: 1, physics: 1, magic: 0, darkness: 0, condition: "player" }, // 亲疏自行
+  "4a9": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "enemy" }, // 雪仇
+  "4ab": { dodge: 1, physics: 1, magic: 0.5, darkness: 0, condition: "enemy" }, // 牵制
+  "4b3": { dodge: 1, physics: 0.5, magic: 1, darkness: 0, condition: "enemy" }, // 昏乱
+  "35c": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "enemy" }, // 武装解除
+  "848": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 体力增加
+  "6b3": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "enemy" }, // 腐臭（臭气）
+  "843": { dodge: 0, physics: 0, magic: 1, darkness: 0, condition: "enemy" }, // 智力精神降低（魔法锤）
+  "9c4": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 龙之力
+  "6ba": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 超硬化
+  "9c0": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 玄结界
+  "847": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 仙人盾
+  "6b7": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 强力守护
+  "842": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 哥布防御
+  "c2": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 铜墙铁盾
+  "c3": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 坚守要塞
+  "c4": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 终极堡垒
+  "35f": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 原初大地
+  "360": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 暗黑之力
+  "78b": { dodge: 1, physics: 1, magic: 1, darkness: 1, condition: "player" }, // 灵魂之青
 };
 export { keigenns };
