7ef5b8bf1a3e8cefdeee9f4c07ad6c9211a7d06f
Souma-Sumire <553469159@qq.com>
2024-07-07 03:21:51 +0800

    refactor(keySkillTimer): 7.0

--- src/keySkillTimer/index.js
+++ src/keySkillTimer/index.js
@@ -77,28 +77,28 @@ if (location.href.includes("localhost") || location.href.includes("127.0.0.1")) 
 
 function faker() {
   show([
-    { id: "0", name: "虚构骑士", job: 19, inParty: true },
-    { id: "0", name: "虚构武僧", job: 20, inParty: true },
-    { id: "0", name: "虚构战士", job: 21, inParty: true },
-    { id: "0", name: "虚构龙骑", job: 22, inParty: true },
-    { id: "0", name: "虚构诗人", job: 23, inParty: true },
-    { id: "0", name: "虚构白魔", job: 24, inParty: true },
-    // { id: "0", name: "虚构黑魔", job: 25, inParty: true },
-    { id: "0", name: "虚构召唤", job: 27, inParty: true },
-    { id: "0", name: "虚构学者", job: 28, inParty: true },
-    { id: "0", name: "虚构忍者", job: 30, inParty: true },
-    { id: "0", name: "虚构机工", job: 31, inParty: true },
-    { id: "0", name: "虚构暗骑", job: 32, inParty: true },
-    { id: "0", name: "虚构占星", job: 33, inParty: true },
-    // { id: "0", name: "虚构武士", job: 34, inParty: true },
-    { id: "0", name: "虚构赤魔", job: 35, inParty: true },
-    // { id: "0", name: "虚构青魔", job: 36, inParty: true },
-    { id: "0", name: "虚构绝枪", job: 37, inParty: true },
-    { id: "0", name: "虚构舞者", job: 38, inParty: true },
-    { id: "0", name: "虚构钐镰", job: 39, inParty: true },
-    { id: "0", name: "虚构贤者", job: 40, inParty: true },
-    { id: "0", name: "虚构蝰蛇", job: 41, inParty: true },
-    { id: "0", name: "虚构画家", job: 42, inParty: true },
+    { id: "0", name: "虚构骑士", job: 19, inParty: true, level: 100 },
+    { id: "0", name: "虚构武僧", job: 20, inParty: true, level: 100 },
+    { id: "0", name: "虚构战士", job: 21, inParty: true, level: 100 },
+    { id: "0", name: "虚构龙骑", job: 22, inParty: true, level: 100 },
+    { id: "0", name: "虚构诗人", job: 23, inParty: true, level: 100 },
+    { id: "0", name: "虚构白魔", job: 24, inParty: true, level: 100 },
+    // { id: "0", name: "虚构黑魔", job: 25, inParty: true ,level:100},
+    { id: "0", name: "虚构召唤", job: 27, inParty: true, level: 100 },
+    { id: "0", name: "虚构学者", job: 28, inParty: true, level: 100 },
+    { id: "0", name: "虚构忍者", job: 30, inParty: true, level: 100 },
+    { id: "0", name: "虚构机工", job: 31, inParty: true, level: 100 },
+    { id: "0", name: "虚构暗骑", job: 32, inParty: true, level: 100 },
+    { id: "0", name: "虚构占星", job: 33, inParty: true, level: 100 },
+    // { id: "0", name: "虚构武士", job: 34, inParty: true ,level:100},
+    { id: "0", name: "虚构赤魔", job: 35, inParty: true, level: 100 },
+    // { id: "0", name: "虚构青魔", job: 36, inParty: true ,level:100},
+    { id: "0", name: "虚构绝枪", job: 37, inParty: true, level: 100 },
+    { id: "0", name: "虚构舞者", job: 38, inParty: true, level: 100 },
+    { id: "0", name: "虚构钐镰", job: 39, inParty: true, level: 100 },
+    { id: "0", name: "虚构贤者", job: 40, inParty: true, level: 100 },
+    { id: "0", name: "虚构蝰蛇", job: 41, inParty: true, level: 100 },
+    { id: "0", name: "虚构画家", job: 42, inParty: true, level: 100 },
   ]);
 }
 function show(party) {
@@ -107,18 +107,19 @@ function show(party) {
   });
   for (const p of party) {
     if (!p.inParty && params.get("inPartyOnly") !== "false") break;
-    for (const key in raidBuffs) {
-      const element = raidBuffs[key];
-      if (element.job.indexOf(p.job) > -1) {
+    for (let index = 0; index < raidBuffs.length; index++) {
+      const item = raidBuffs[index];
+      if (item.job.includes(p.job)) {
         let art = document.createElement("article");
-        const action = getAction(key, getLevels[p.id]?.level ?? 999);
-        art.style.order = element.order;
+        const id = typeof item.id === 'function' ? item.id(p.level) : item.id;
+        const action = getAction(id, getLevels[p.id]?.level ?? 999);
+        art.style.order = index;
         art.classList.add("useful_" + action.Useful);
-        art.setAttribute("data-from", `${p.id}-${key}`);
+        art.setAttribute("data-from", `${p.id}-${id}`);
         let aside = document.createElement("aside");
-        const recast = typeof element.recast1000ms === 'function' ? element.recast1000ms(p.level) : element.recast1000ms;
+        const recast = typeof item.recast1000ms === 'function' ? item.recast1000ms(p.level) : item.recast1000ms;
         aside.setAttribute("data-recast", recast);
-        aside.setAttribute("data-duration", element.duration);
+        aside.setAttribute("data-duration", item.duration);
         aside.innerText = "";
         art.append(aside);
         let section = document.createElement("img");
@@ -133,7 +134,7 @@ function show(party) {
         art.append(shadow);
         if (
           party?.reduce((pre, cur) => {
-            if (cur.job === p.job || element.job.find((value) => value === cur.job)) pre++;
+            if (cur.job === p.job || item.job.find((value) => value === cur.job)) pre++;
             return pre;
           }, 0) >= 2
         ) {
@@ -143,7 +144,7 @@ function show(party) {
           masterName.classList.add(job.en);
           art.append(masterName);
         }
-        document.querySelector(`#no${element.type}`).append(art);
+        document.querySelector(`#no${item.type}`).append(art);
 
         art.use = function () {
           let recast = aside.getAttribute("data-recast");
