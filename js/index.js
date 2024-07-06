1f34a2a249f88dea17c3d72346e61f7e5cfcf18f
Souma-Sumire <Souma-Sumire@users.noreply.github.com>
2024-07-06 19:22:12 +0000

    deploy: 7ef5b8bf1a3e8cefdeee9f4c07ad6c9211a7d06f

--- js/index.js
+++ js/index.js
@@ -1,1 +1,1 @@
-!function(){var e=document.getElementById("s"),t=3;setInterval((function(){e.innerHTML=--t}),1e3),setTimeout((function(){location.href="https://souma.diemoe.net/ff14-overlay-vue/#/"}),3e3)}();
+(()=>{const e=document.getElementById("s");let t=3;setInterval((()=>{e.innerHTML=--t}),1e3),setTimeout((()=>{location.href="https://souma.diemoe.net/ff14-overlay-vue/#/"}),3e3)})();
