3140a7dac74c4d35b8c3893dbb25637334aea17a
Souma-Sumire <553469159@qq.com>
2024-07-09 01:20:06 +0800

    fix: a lot bug

--- src/keySkillTimer/index.js
+++ src/keySkillTimer/index.js
@@ -3,7 +3,7 @@ import { getAction } from "../../resources/data/actions";
 import { compareSame } from "../../resources/function/compareSameGroup";
 import "../../resources/function/loadComplete";
 import { logProcessing } from "../../resources/function/logProcessing";
-import { TTS } from "../../resources/function/TTS";
+import { TTS as tts } from "../../resources/function/TTS";
 import "../../resources/function/xianyu";
 import "./index.scss";
 import { raidBuffs国服, raidBuffs国际服 } from "./raidbuffs";
@@ -16,8 +16,19 @@ let timers = [];
 let party = [];
 let youID = null;
 let inFaker = true;
-// let TTSLast1S = new Set();
-const raidBuffs = params.get("international") === "true" ? raidBuffs国际服 : raidBuffs国服;
+
+
+const TTS_CONFIG = [
+  params.get("dajinengTTS") !== "false",
+  params.get("jianshangTTS") !== "false",
+  params.get("tuanfuTTS") !== "false"
+]
+
+const MODE = params.get("international") === "true" ? 'global' : 'cn'
+
+const RAID_BUFFS = MODE === 'global' ? raidBuffs国际服 : raidBuffs国服;
+
+console.log(`当前处于${MODE === 'global' ? '国际服' : '国服'}模式`)
 
 addOverlayListener("ChangePrimaryPlayer", (e) => (youID = e.charID.toString(16).toUpperCase()));
 addOverlayListener("PartyChanged", (e) => {
@@ -32,29 +43,33 @@ addOverlayListener("LogLine", (e) => {
   if (e.line[0] === "21" || (e.line[0] === "22" && e.line[45] === "0")) {
     let log = logProcessing(e.line, "action");
     let actionID = parseInt(log["actionID"], 16);
-    if (party.some((p) => p.inParty && p.id === log["casterID"]) && raidBuffs[actionID]) {
-      document.querySelector(`article[data-from="${log["casterID"]}-${compareSame(actionID)}"]`)?.use();
-      doTTS(actionID);
-    } else if (log["casterID"] === youID && raidBuffs[actionID] !== undefined) {
-      doTTS(actionID);
+    const member = document.querySelector(`article[data-from="${log["casterID"]}-${compareSame(actionID)}"]`)
+    if (member) {
+      member.use();
+      if (TTS_CONFIG[member.getAttribute("data-type")])
+        doTTS(member.getAttribute("data-tts"));
+    } else if (log["casterID"] === youID) {
+      const buff = RAID_BUFFS.find(v => v.id === actionID);
+      if (buff && TTS_CONFIG[buff.type] && buff.tts) {
+        doTTS(buff.tts);
+      }
     }
   } else if (e.line[0] === "33" && e.line[3] === "4000000F") resetEverything();
 });
 addOverlayListener("ChangeZone", () => resetEverything());
+
+function doTTS(string) {
+  if (string) {
+    tts(string);
+  }
+}
+
 function resetEverything() {
   document.querySelectorAll("body > article > article").forEach((article) => {
     article?.cancel();
   });
 }
-function doTTS(actionID) {
-  if (
-    (raidBuffs[actionID]?.type === 0 && params.get("dajinengTTS") !== "false") ||
-    (raidBuffs[actionID]?.type === 1 && params.get("jianshangTTS") !== "false") ||
-    (raidBuffs[actionID]?.type === 2 && params.get("tuanfuTTS") !== "false")
-  ) {
-    TTS(raidBuffs[actionID]?.tts);
-  }
-}
+
 document.addEventListener("onOverlayStateUpdate", (e) => {
   if (e.detail.isLocked) {
     inFaker = false;
@@ -70,9 +85,11 @@ if (location.href.includes("localhost") || location.href.includes("127.0.0.1")) 
   faker();
 }
 
-// document.querySelectorAll("body > article > article").forEach((element) => {
-//   element.use();
-// });
+// setTimeout(() => {
+//   document.querySelectorAll("body > article > article").forEach((element) => {
+//     element.use();
+//   });
+// }, 1000);
 // setTimeout(() => resetEverything(), 3000);
 
 function faker() {
@@ -83,16 +100,16 @@ function faker() {
     { id: "0", name: "虚构龙骑", job: 22, inParty: true, level: 100 },
     { id: "0", name: "虚构诗人", job: 23, inParty: true, level: 100 },
     { id: "0", name: "虚构白魔", job: 24, inParty: true, level: 100 },
-    // { id: "0", name: "虚构黑魔", job: 25, inParty: true ,level:100},
+    { id: "0", name: "虚构黑魔", job: 25, inParty: true, level: 100 },
     { id: "0", name: "虚构召唤", job: 27, inParty: true, level: 100 },
     { id: "0", name: "虚构学者", job: 28, inParty: true, level: 100 },
     { id: "0", name: "虚构忍者", job: 30, inParty: true, level: 100 },
     { id: "0", name: "虚构机工", job: 31, inParty: true, level: 100 },
     { id: "0", name: "虚构暗骑", job: 32, inParty: true, level: 100 },
     { id: "0", name: "虚构占星", job: 33, inParty: true, level: 100 },
-    // { id: "0", name: "虚构武士", job: 34, inParty: true ,level:100},
+    { id: "0", name: "虚构武士", job: 34, inParty: true, level: 100 },
     { id: "0", name: "虚构赤魔", job: 35, inParty: true, level: 100 },
-    // { id: "0", name: "虚构青魔", job: 36, inParty: true ,level:100},
+    { id: "0", name: "虚构青魔", job: 36, inParty: true, level: 100 },
     { id: "0", name: "虚构绝枪", job: 37, inParty: true, level: 100 },
     { id: "0", name: "虚构舞者", job: 38, inParty: true, level: 100 },
     { id: "0", name: "虚构钐镰", job: 39, inParty: true, level: 100 },
@@ -101,25 +118,35 @@ function faker() {
     { id: "0", name: "虚构画家", job: 42, inParty: true, level: 100 },
   ]);
 }
+
+function getProp(prop, player) {
+  return typeof prop === "function" ? prop(player.level) : prop;
+}
+
 function show(party) {
   document.querySelectorAll("body > article").forEach((article) => {
     article.innerHTML = "";
   });
   for (const p of party) {
     if (!p.inParty && params.get("inPartyOnly") !== "false") break;
-    for (let index = 0; index < raidBuffs.length; index++) {
-      const item = raidBuffs[index];
+    for (let index = 0; index < RAID_BUFFS.length; index++) {
+      const item = RAID_BUFFS[index];
       if (item.job.includes(p.job)) {
-        let art = document.createElement("article");
+        const art = document.createElement("article");
         const id = typeof item.id === 'function' ? item.id(p.level) : item.id;
         const action = getAction(id, getLevels[p.id]?.level ?? 999);
         art.style.order = index;
         art.classList.add("useful_" + action.Useful);
         art.setAttribute("data-from", `${p.id}-${id}`);
-        let aside = document.createElement("aside");
-        const recast = typeof item.recast1000ms === 'function' ? item.recast1000ms(p.level) : item.recast1000ms;
+        const aside = document.createElement("aside");
+        const recast = getProp(item.recast1000ms, p);
+        const duration = getProp(item.duration, p);
+        const tts = getProp(item.tts, p);
+        const type = getProp(item.type, p);
         aside.setAttribute("data-recast", recast);
-        aside.setAttribute("data-duration", item.duration);
+        aside.setAttribute("data-duration", duration);
+        aside.setAttribute("data-type", type);
+        aside.setAttribute("data-tts", tts);
         aside.innerText = "";
         art.append(aside);
         let section = document.createElement("img");
