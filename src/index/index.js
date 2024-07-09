32728a7da722757dea1b2931d6b30e4d44c2160a
Souma-Sumire <553469159@qq.com>
2024-07-09 22:19:42 +0800

    chore

--- src/index/index.js
+++
@@ -1,8 +0,0 @@
-const s = document.getElementById("s");
-let i = 3;
-setInterval(() => {
-  s.innerHTML = --i;
-}, 1000);
-setTimeout(() => {
-  location.href = "https://souma.diemoe.net/ff14-overlay-vue/#/";
-}, 3000);
